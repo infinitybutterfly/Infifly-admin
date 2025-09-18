acd45ea05c5cb9a3da607709c5e138d634ecaa27
ragnarokas47 <153199958+ragnarokas47@users.noreply.github.com>
2025-09-18 18:40:43 +0530

    intial commit

---
+++ app/src/main/java/com/example/admindiyabatti/utils/Constants.kt
@@ -0,0 +1,120 @@
+package com.example.admindiyabatti.utils
+
+import com.example.admindiyabatti.R
+
+object Constants {
+
+      val allProductsCategory = arrayOf(
+//        "Baby care ",
+//        "Vegetable & Fruits ",
+//        "Dairy & Breakfast",
+//        "Munchies",
+//        "Cold Drinks",
+//        "Instant & Frozen Food",
+//        "Tea Coffer & Health Drinks",
+//        "Bakery & Biscuits",
+//        "Sweet Tooth",
+//        "Atta Rice & Dal",
+//        "Dry Fruits Masala & Oil",
+//        "Sauces & Spreads",
+//        "Chicken Meat & Fish",
+//        "Pan Corner",
+//        "Organic & Premium",
+//        "Baby Care",
+//        "Pharma & Wellness",
+//        "Cleaning Essential",
+//        "Home & Office",
+//        "Personal Care",
+//        "Pet Care",
+//        "Bakery & Biscuits",
+//        "Sweet Tooth",
+//        "Atta Rice & Dal",
+//        "Dry Fruits Masala & Oil",
+//        "Sauces & Spreads",
+//        "Chicken Meat & Fish",
+//        "Pan Corner",
+//        "Organic & Premium",
+//        "Baby Care",
+//        "Pharma & Wellness",
+//        "Cleaning Essential",
+//        "Home & Office",
+//        "Personal Care",
+//        "Pet Care",
+
+          "Skills",
+          "Motors",
+          "Cables & Wires",
+          "Electric/Circuit Boards",
+          "Capacitors",
+          "Atta Rice & Dal",
+          "Baby care",
+          "Bakery & Biscuits",
+          "Chicken Meat & Fish",
+          "Cleaning Essential",
+          "Cold Drinks",
+          "Dairy & Breakfast",
+          "Dry Fruits & Masala",
+          "Oil",
+          "Home & Office",
+          "Instant & Frozen Food",
+          "Munchies",
+          "Energy Drinks",
+          "Personal Care",
+          "Pet Care",
+          "Sauces & Spreads",
+          "Sweet Tooth",
+          "Tea Coffer & Health Drinks",
+          "Vegetable & Fruits ",
+
+          )
+    val allProductsCategoryIcon = arrayOf(
+        R.drawable.skill,
+        R.drawable.motor,
+        R.drawable.wire,
+        R.drawable.circuit_board,
+        R.drawable.capacitor,
+        R.drawable.atta_rice,
+        R.drawable.baby,
+        R.drawable.bakery_biscuits,
+        R.drawable.chicken_meat,
+        R.drawable.cleaning_products,
+        R.drawable.cold_and_juices,
+        R.drawable.dairy_breakfast,
+        R.drawable.dryfam,
+        R.drawable.organic_premium,
+        R.drawable.home_office,
+        R.drawable.instant_frozen,
+        R.drawable.munchies,
+        R.drawable.energy_drink_603884,
+        R.drawable.self,
+        R.drawable.pet_care,
+        R.drawable.sauce_spreads,
+        R.drawable.sweet_tooth,
+        R.drawable.tea_coffee,
+        R.drawable.vegetable
+    )
+    val allUnitsOfProducts = arrayOf(
+        "kg",
+        "gm",
+        "ml",
+        "Ltr",
+        "Packets",
+        "Places",
+        "Time"
+
+        )
+
+    val allProductType = arrayOf(
+        "Soft Skills",
+        "Hard Skills",
+        "Liquid",
+        "Solid",
+        "Glass",
+        "Gas",
+        "Delicate"
+
+        )
+
+
+
+}
